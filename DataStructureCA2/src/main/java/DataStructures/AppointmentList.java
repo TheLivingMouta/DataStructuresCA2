daea01fc160605f978034bc61f774b64f4aced9c
Oran <student@ACCESS-491B0N.ad.dkit.ie>
2023-05-07 18:49:57 +0100

    set, remove and isEmpty methods added to AppointmentList

--- DataStructureCA2/src/main/java/DataStructures/AppointmentList.java
+++ DataStructureCA2/src/main/java/DataStructures/AppointmentList.java
@@ -105,4 +105,76 @@ public class AppointmentList
         return true;
     }
 
+    /**
+     * Replaces the appointment at the specified index in this linked list with the specified appointment.
+     *
+     * @param index the index of the appointment to replace
+     * @param appointment the appointment to be stored at the specified index
+     * @return the appointment previously at the specified position
+     * @throws IllegalArgumentException if the appointment field is null
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public Appointment set(int index, Appointment appointment) {
+        if (appointment == null) {
+            throw new IllegalArgumentException("Appointment cannot be null"); //throws IllegalArgumentException if the appoinment field is null
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid position provided");// invalid index, output Invalid position provided
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        Appointment oldData = current.data; // save the current appointment object
+        current.data = appointment; // replace it with the new appointment object
+        return oldData; // return the old appointment object;
+    }
+
+    /**
+     * Removes the first occurrence of the specified appointment from this linked list, if it is present.
+     *
+     * @param appointment the appointment to be removed from this list, if present
+     * @return true if this list contained the specified appointment, false otherwise
+     * @throws IllegalArgumentException if the appointment field is null
+     */
+    public boolean remove(Appointment appointment) {
+        if (appointment == null) {
+            throw new IllegalArgumentException("Appointment cannot be null"); //throws IllegalArgumentException if the appoinment field is null
+        }
+        if (head == null) { // list is empty
+            return false;
+        }
+        if (head.data.equals(appointment)) {
+            head = head.next;  // remove head node
+            if (head == null) {  // list is now empty
+                tail = null;
+            }
+            size--; //decrement the size
+            return true;
+        }
+        Node current = head;
+        while (current.next != null && !current.next.data.equals(appointment)) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next; // remove node after current
+            if (current.next == null) {
+                tail = current; // update tail reference
+            }
+            size--; //decrement the size
+            return true;
+        }
+        return false; // appointment not found
+    }
+
+    /**
+     *
+     * Checks if the linked list is empty.
+     *
+     * @return true if the list is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return head == null; // list is empty if head is null
+    }
 }
