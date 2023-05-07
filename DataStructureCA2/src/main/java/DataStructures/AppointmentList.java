aada4d4b1e312b5dd0e88897d019ea002df6b4eb
Oran <student@ACCESS-491B0N.ad.dkit.ie>
2023-05-07 21:30:48 +0100

    Changed the access modifiers from private to protected

--- DataStructureCA2/src/main/java/DataStructures/AppointmentList.java
+++ DataStructureCA2/src/main/java/DataStructures/AppointmentList.java
@@ -4,7 +4,7 @@
  */
 package DataStructures;
 
-import AppData.appointment;
+import AppData.Appointment;
 
 /**
  *
@@ -13,17 +13,17 @@ import AppData.appointment;
 public class AppointmentList
 {
 
-    private Node head; // the first node in the list
-    private Node tail; // the last node in the list
-    private int size; // the number of appointments in the list
+    protected Node head; // the first node in the list
+    protected Node tail; // the last node in the list
+    protected int size; // the number of appointments in the list
 
-    private static class Node
+    protected static class Node
     {
 
-        appointment data; // the appointment object stored in this node
-        Node next; // the reference to the next node in the list
+        protected Appointment data; // the appointment object stored in this node
+        protected Node next; // the reference to the next node in the list
 
-        private Node(appointment data) {
+        protected Node(Appointment data) {
             this.data = data;
             this.next = null;
         }
