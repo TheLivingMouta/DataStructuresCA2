7b452b2c26a38448e8da7567280b4f0b7cfd21d3
Oran <student@ACCESS-491B0N.ad.dkit.ie>
2023-05-07 21:29:07 +0100

    Overrrided the add method from AppoinmentList to make PrioryQueue

--- DataStructureCA2/src/main/java/DataStructures/PriorityQueue.java
+++ DataStructureCA2/src/main/java/DataStructures/PriorityQueue.java
@@ -4,10 +4,47 @@
  */
 package DataStructures;
 
+import AppData.Appointment;
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author carol
  */
-public class PriorityQueue {
-    
+public class PriorityQueue extends AppointmentList
+{
+
+    @Override
+    public boolean add(Appointment appointment) {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+
+        Node newNode = new Node(appointment);
+        Node current = head.next;
+        Node previous = head;
+        for (int i = 0; i < size(); i++) {
+            if (tail.data.equals(newNode.data)) {
+                newNode.next = tail;
+                tail = newNode;
+            } else if (head.data.equals(newNode.data)) {
+                newNode.next = head;
+                head = newNode;
+            } else {
+                while (current.data.equals(newNode.data)) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = newNode;
+                newNode.next = current;
+
+            }
+        }
+
+        size++;
+        return false;
+
+    }
+
 }
